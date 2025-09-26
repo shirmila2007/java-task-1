24c57d90f27bccf2743823900b5e721c5a9730f7
shirmila2007 <shirmila2007@gmail.com>
2025-09-26 14:01:58 +0530

    second commit

--- task1/voting.js
+++ task1/voting.js
@@ -1,31 +1,20 @@
-function librarySystem() {
-  let username = prompt("Enter username:");
-  let password = prompt("Enter password:");
+function votingChecker() {
+  let again = true;
 
-  if (username === "library" && password === "book123") {
-    let borrow = confirm("Do you want to borrow a book?");
-    if (borrow) {
-      let choice = prompt("Choose a category:\n1 = Fiction\n2 = Science\n3 = History");
-      switch (choice) {
-        case "1":
-          alert("You selected Fiction");
-          break;
-        case "2":
-          alert("You selected Science");
-          break;
-        case "3":
-          alert("You selected History");
-          break;
-        default:
-          alert("Invalid category");
-      }
+  while (again) {
+    let name = prompt("Enter your name:");
+    let age = prompt("Enter your age:");
+
+    if (age >= 18) {
+      alert("Hello" + name + "you are eligible to vote");
     } else {
-      alert("Maybe next time! Goodbye!");
+      alert("Sorry" + name + "you are not eligible to vote")
     }
-  } else {
-    alert("Invalid login!");
+
+    again = confirm("Do you want to check again?");
   }
+
+  alert("Goodbye!");
 }
 
-
-librarySystem();
+votingChecker();
